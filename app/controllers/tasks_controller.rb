5dad14d82463c44ff5bc867c6fccd98cb487897e
yuuko kataoka <ngnl.ussan@gmail.com>
2017-07-12 08:34:25 +0000

    update tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
     def update
         @task = Task.find(params[:id])
         
-        if @task.update(message_params)
+        if @task.update(task_params)
             flash[:success] = 'Task は正常に更新されました'
             redirect_to @task
         else
