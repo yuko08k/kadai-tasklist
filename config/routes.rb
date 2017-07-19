674f3d8e0ec8df43cc398689d00c58dc1ed2c520
yuuko kataoka <ngnl.ussan@gmail.com>
2017-07-19 13:44:55 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root to: 'tasks#index'
+  root to: 'tasks#index'
     
-    get 'signup', to: 'tasks#new'
-  resources :tasks, only: [:index, :show, :new, :create]
+  get 'signup', to: 'tasks#new'
+  resources :tasks
 end
